61787036225ec8507f5e7368cc0aba0959066b5e
cjberg28 <cjberg28@yahoo.com>
2022-07-14 22:48:12 -0500

    Moving all functionality from previous Angular project to new one.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import all PrimeNG modules here!!
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    //Add all PrimeNG modules here so they can be used in your application!!
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
